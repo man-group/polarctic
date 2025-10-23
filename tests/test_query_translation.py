0a03733a668545a58a7e5e8d89065413e0db2b57
Johan Mabille <johan.mabille@gmail.com>
2025-10-23 18:00:01 +0200

    More tests for query transmator

--- tests/test_query_translation.py
+++ tests/test_query_translation.py
@@ -3,10 +3,109 @@ from arcticdb import QueryBuilder
 import polars as pl
 import pytest
 
-def test_col(translator):
+def test_greater(translator):
     q = QueryBuilder()
     q = translator.translate(pl.col("col1") > 2, q)
     qe = QueryBuilder()
     qe = qe[qe["col1"] > 2]
-
     assert q == qe
+
+def test_greater_equal(translator):
+    q = QueryBuilder()
+    q = translator.translate(pl.col("col1") >= 2, q)
+    qe = QueryBuilder()
+    qe = qe[qe["col1"] >= 2]
+    assert q == qe
+
+def test_less(translator):
+    q = QueryBuilder()
+    q = translator.translate(pl.col("col1") < 2, q)
+    qe = QueryBuilder()
+    qe = qe[qe["col1"] < 2]
+    assert q == qe
+
+def test_less_equal(translator):
+    q = QueryBuilder()
+    q = translator.translate(pl.col("col1") <= 2, q)
+    qe = QueryBuilder()
+    qe = qe[qe["col1"] <= 2]
+    assert q == qe
+
+def test_equal(translator):
+    q = QueryBuilder()
+    q = translator.translate(pl.col("col1") == 2, q)
+    qe = QueryBuilder()
+    qe = qe[qe["col1"] == 2]
+    assert q == qe
+
+def test_not_equal(translator):
+    q = QueryBuilder()
+    q = translator.translate(pl.col("col1") != 2, q)
+    qe = QueryBuilder()
+    qe = qe[qe["col1"] != 2]
+    assert q == qe
+
+def test_add(translator):
+    q = QueryBuilder()
+    q = translator.translate(pl.col("col1") + pl.col("col2"), q)
+    qe = QueryBuilder()
+    qe = qe[qe["col1"] + qe["col2"]]
+    assert q == qe
+
+def test_subtract(translator):
+    q = QueryBuilder()
+    q = translator.translate(pl.col("col1") - pl.col("col2"), q)
+    qe = QueryBuilder()
+    qe = qe[qe["col1"] - qe["col2"]]
+    assert q == qe
+
+def test_multiply(translator):
+    q = QueryBuilder()
+    q = translator.translate(pl.col("col1") * pl.col("col2"), q)
+    qe = QueryBuilder()
+    qe = qe[qe["col1"] * qe["col2"]]
+    assert q == qe
+
+def test_divide(translator):
+    q = QueryBuilder()
+    q = translator.translate(pl.col("col1") / pl.col("col2"), q)
+    qe = QueryBuilder()
+    qe = qe[qe["col1"] / qe["col2"]]
+    assert q == qe
+
+def test_modulo(translator):
+    q = QueryBuilder()
+    with pytest.raises(NotImplementedError):
+        q = translator.translate(pl.col("col1") % pl.col("col2"), q)
+
+# TODO: fix
+#def test_negation(translator):
+#    q = QueryBuilder()
+#    q = translator.translate(-pl.col("col1"), q)
+#    qe = Querybuilder()
+#    qe = qe[-qe["col1"]]
+#    assert q == qe
+
+def test_and(translator):
+    q = QueryBuilder()
+    q = translator.translate((pl.col("col1") > 2) & (pl.col("col2") < 3), q)
+    qe = QueryBuilder()
+    qe = qe[(qe["col1"] > 2) & (qe["col2"] < 3)]
+    assert q == qe
+
+
+def test_or(translator):
+    q = QueryBuilder()
+    q = translator.translate((pl.col("col1") > 2) | (pl.col("col2") < 3), q)
+    qe = QueryBuilder()
+    qe = qe[(qe["col1"] > 2) | (qe["col2"] < 3)]
+    assert q == qe
+
+# TODO: fix
+#def test_not(translator):
+#    q = QueryBuilder()
+#    q = translator.translate(~pl.col("col1"), q)
+#    qe = QueryBuilder()
+#    qe = qe[~qe["col1"]]
+#    assert q == qe
+
