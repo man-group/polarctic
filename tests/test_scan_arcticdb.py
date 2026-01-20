a940f0ce38305429913d7ef54fafb8f265a1e0b2
Johan Mabille <johan.mabille@gmail.com>
2026-01-20 17:11:53 +0100

    Added disclosure

--- tests/test_scan_arcticdb.py
+++ tests/test_scan_arcticdb.py
@@ -1,3 +1,12 @@
+
+"""
+Copyright 2026 Man Group Operations Limited
+
+Use of this software is governed by the Business Source License 1.1 included in the file LICENSE
+
+As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+"""
+
 """
 Unit tests for polarctic.scan_arcticdb using a real ArcticDB LMDB-backed store.
 
